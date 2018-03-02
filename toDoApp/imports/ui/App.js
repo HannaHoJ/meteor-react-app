645939f8d570f05367f9249e57032bc6f63099dd
Johanna D <johanna.dietzz@gmail.com>
2018-03-02 14:27:04 +0100

    added tasks remove and list tasks

--- toDoApp/imports/ui/App.js
+++ toDoApp/imports/ui/App.js
@@ -6,39 +6,62 @@ import Task from './Task.js';
  
 // App component - represents the whole app
 class App extends Component {
-  // getTasks() {
-  //   return [
-  //     { _id: 1, text: 'This is task 1' },
-  //     { _id: 2, text: 'This is task 2' },
-  //     { _id: 3, text: 'This is task 3' },
-  //   ];
-  // }
+	constructor(props){
+		super(props)
+		this.submitForm = this.submitForm.bind(this);
+		this.state = {
+			text: "",
+		}
+	}
  
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+	renderTasks() {
+		return this.props.tasks.map((task) => (
+			<Task key={task._id} taskId={task._id} task={task} />
+		));
+	}
  
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+ 	submitForm(e){
+ 		e.preventDefault();
+ 		const text = this.state.text;
+ 		console.log(text);
+ 		this.setState({
+		    text: ''
+		});
+ 		Tasks.insert({
+ 			text: text, //= text: text
+ 			createdAt: new Date()
+ 		})
+ 		
+ 	}
+
+	render() {
+		return (
+			<div className="container">
+				<header>
+				  <h1>Todo List</h1>
+				</header>
+					<form onSubmit={(e) => { this.submitForm(e)} }>
+						<input 
+							type="text" 
+							ref="clear" //usage with React.findDOMNode(this.refs.<ref_value>) 
+							onChange={(e) => this.setState({ text: e.target.value.trim() })} 
+							placeholder="Was willst du heute machen?" 
+							size='50'
+							id="task-input" required/>
+					</form>
+				<ul>
+				  {this.renderTasks()}
+				</ul>
+			</div>
+		);
+	}
 }
 
 //bit like apollo?
+//DB query to list all the tasks
 export default withTracker(() => {
   return {
-  	//defines a prop tasks with can be used to map through
-    tasks: Tasks.find({}).fetch(),
+  	//defines a prop tasks with can be used to map throughs
+    tasks: Tasks.find({}, { sort: {createdAt: -1} }).fetch(),
   };
 })(App);
--- toDoApp/imports/ui/Task.js
+++ toDoApp/imports/ui/Task.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'; //import react specific Modules
 
-// export default class Task extends Component{
+import { Tasks } from '../api/tasks.js';
 
-// 	render(){
-// 		return(
-// 			<li>
-// 				{ this.props.task.text }
-// 			</li>
-// 		)
-// 	}
-// } 
+class Task extends Component{
+	constructor(props){
+		super(props);
+		this.deleteTask = this.deleteTask.bind(this);
+		this.checkedTask = this.checkedTask.bind(this);
+	}
 
-const Task = (props) => { 
-	return <li> {props.task.text } </li> 
-}
+	checkedTask(){
+		Tasks.update( this.props.taskId, {
+			$set: { checked: !this.props.task.checked}
+		})
+	}
+
+	deleteTask(){
+		Tasks.remove(this.props.taskId);
+	}
+
+	render(){
+		return( 
+		<li className={ this.props.task.checked? 'checked': '' }> 
+			<input 
+			type="checkbox"
+			checked={!!this.props.task.checked}
+			onClick={ this.checkedTask }
+			/>
+			<span>{this.props.task.text } </span>
+			<button onClick={ this.deleteTask } className="delete">X</button>
+		</li> 
+	)}
+} 
+
+
 
 export default Task;
